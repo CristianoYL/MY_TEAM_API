2d57949cb9f88632c9558e4bf989dfdd63d6a498
CristianoYL <li.yin.cristiano@rutgers.edu>
2017-04-03 18:35:23 -0400

    added heroku deployment required files and uwsgi related files

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'myteam'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app,authenticate,identity)    #/auth
 
 api.add_resource(Player,'/player/<string:email>')
