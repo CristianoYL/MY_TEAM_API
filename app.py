6847b5dd28f45e025de19b99f63725e06494df09
CristianoYL <li.yin.cristiano@rutgers.edu>
2017-05-30 19:22:51 -0400

    added PlayerRegistration PUT API

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ api.add_resource(ResultByTournamentClub,'/result/tournament/<string:tournamentID
 if __name__ == '__main__' :
     from db import db
     db.init_app(app)
-    app.run(host = '192.168.1.5',port = 5000,debug=True)
+    app.run(host = '192.168.1.9',port = 5000,debug=True)
--- resources/player.py
+++ resources/player.py
@@ -99,9 +99,9 @@ class PlayerList(Resource):
 
 class PlayerRegistration(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('firstName', type=str, required=False)
-    parser.add_argument('lastName', type=str, required=False)
-    parser.add_argument('displayName', type=str, required=True, help="The player's firtname cannot be blank.")
+    parser.add_argument('firstName', type=str, required=True,help="The player's firstname cannot be blank.")
+    parser.add_argument('lastName', type=str, required=True,help="The player's lastname cannot be blank.")
+    parser.add_argument('displayName', type=str, required=True, help="The player's display name cannot be blank.")
     parser.add_argument('role', type=str, required=True,help="The player's role cannot be blank.")
     parser.add_argument('age', type=int, required=False)
     parser.add_argument('height',type=float, required=False)
@@ -113,19 +113,37 @@ class PlayerRegistration(Resource):
     def post(self,clubID): # create a new player in this club
         data = self.parser.parse_args()
 
-        # check full name (if given)
-        if data['firstName']:
-            if data['lastName']:
-                playersWithSameFullName = PlayerModel.find_by_fullname(data['firstName'],data['lastName'])
-                if playersWithSameFullName.first(): # if players with same full name exists
-                    #   return these players to client and let them decide what to do next
-                    return {"players":[player.json() for player in playersWithSameFullName]}, 400
+        # check full name
+        playersWithSameFullName = PlayerModel.find_by_fullname(data['firstName'],data['lastName'])
+        if playersWithSameFullName.first(): # if players with same full name exists
+            #   return these players to client and let them decide what to do next
+            return {"players":[player.json() for player in playersWithSameFullName]}, 400
 
-        # check display name
-        playersWithSameDisplayName = PlayerModel.find_by_display_name(data['displayName'])
-        if playersWithSameDisplayName.first():   # if players with same displayer name exists
-            return {"players":[player.json() for player in playersWithSameDisplayName]}, 400
+        player = PlayerModel(None,None,**data)
+        try:
+            player.save_to_db()
+            try:
+                current_date = date.today()
+                teamsheet = TeamsheetModel(clubID,player.id,current_date,True,False)
+                teamsheet.save_to_db()
+                return {
+                    'player' : player.json(),
+                    'teamsheet' : teamsheet.json()
+                    }, 201
+            except:
+                try:
+                    player.delete_from_db()
+                except:
+                    traceback.print_exc()
+                    return {'message':'Internal server error! Roll back error!.'},500
+                traceback.print_exc()
+                return {'message':'Internal server error! Failed to add player into club teamsheet.'},500
+        except:
+            traceback.print_exc()
+            return {'message':'Internal server error! Failed to create player'},500
 
+    def put(self,clubID):   # force to create a new player in this club
+        data = self.parser.parse_args()
         player = PlayerModel(None,None,**data)
         try:
             player.save_to_db()
