557d32f78add93309aeaf4b792f2f101458bc947
CristianoYL <li.yin.cristiano@rutgers.edu>
2017-07-20 14:49:40 -0400

    Minor bug fix, to deploy on Heroku.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
-import config
+# import config
 
 from resources.user import User,UserUpdate
 from resources.player import PlayerByUser,PlayerByToken,PlayerByID,PlayerList,PlayerRegistration
